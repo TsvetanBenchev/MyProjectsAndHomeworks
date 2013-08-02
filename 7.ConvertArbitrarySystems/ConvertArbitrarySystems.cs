a17fd87ce155a4356f8821cdf0278801d03a85b2
TsvetanBenchev <djcro@abv.bg>
2013-08-02 11:59:38 +0300

    A program in C# which converts numbers.
    Works with numeral systems with base 2, 8, 10 and 16.

---
+++ 7.ConvertArbitrarySystems/ConvertArbitrarySystems.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class ConvertArbitrarySystems
+{
+    static void Main(string[] args)
+    {
+        // Works for 2, 8, 10 and 16 base.
+        Console.Write("Enter the base of the system you would like to convert from: ");
+        int s = int.Parse(Console.ReadLine());
+        if ( s < 2)
+        {
+            Console.WriteLine("Incorrect input!");
+        }
+        Console.Write("Enter base to convert to: ");
+        int d = int.Parse(Console.ReadLine());
+        if (d > 16)
+        {
+            Console.WriteLine("Incorrect input!");
+        }
+
+        Console.WriteLine("Enter your number: ");
+        string binary = Console.ReadLine();
+        string result = Convert.ToString(Convert.ToInt32(binary, s), d);
+        Console.Write("Result: " + result);
+        Console.WriteLine();
+    }
+}
